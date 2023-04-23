8b2c0adceaafb204f1e41fb3459e2d0747c1adcb
Susanta <inbox.susanta@gmail.com>
2023-04-23 17:22:37 +0530

    Make toggle button working

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -63,6 +63,46 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
             <img src="../assets/menu-icon.svg" alt="menu-icon" />
           </button>
         )}
+
+        {/* Mobile Menu Popup */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* Close Icon */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img src="../assets/close-icon.svg" alt="close-icon" />
+              </button>
+            </div>
+            {/* Menu Items */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
