9682daa7f02f676744565389d8f22b08fba804e4
Susanta <inbox.susanta@gmail.com>
2023-04-29 20:11:27 +0530

    Added contact section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LineGradient from './components/LineGradient';
 import MySkills from './scenes/MySkills';
 import Projects from './scenes/Projects';
 import Testimonials from './scenes/Testimonials';
+import Contact from './scenes/Contact';
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
@@ -49,6 +50,10 @@ function App() {
         <LineGradient />
         <div className="w-5/6 mx-auto md:h-full">
           <Testimonials />
+        </div>
+        <LineGradient />
+        <div className="w-5/6 mx-auto md:h-full">
+          <Contact />
         </div>
       </div>
     </div>
